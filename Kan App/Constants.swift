7e1708e36c62f0379140a8708db154771d8a1f26
HudahanAltn <dev.hudahanaltun@gmail.com>
2022-11-29 01:04:08 +0300

    girisVC constraint öncesi son yapılandırma.

---
+++ Kan App/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Kan App
+//
+//  Created by Hüdahan Altun on 29.11.2022.
+//
+
+import Foundation
+
+
+struct K{
+    
+    static let gToSifre:String = "girisToSifreYen"
+    static let gToYasal:String = "girisToYasal"
+    static let yToKisi:String = "yasalToKisisel"
+    static let kToKayit:String = "kisiseltoKayit"
+    static let kToMain:String = "kayitToMain"
+    static let gToMain:String = "girisToMain"
+}
--- Kan App/ViewController.swift
+++ Kan App/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sifreUnButton: UIBarButtonItem!
+    
+    @IBOutlet weak var appİmage: UIImageView!
+    
+    
+    @IBOutlet weak var appName: UILabel!
+    
+    
+    @IBOutlet weak var twoSegmentedView: UISegmentedControl!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var sifreTextField: UITextField!
+    
+    
+    @IBOutlet weak var girisButton: UIButton!
+    
+    
+    @IBOutlet weak var kayıtButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func girisButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func kayıtButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func sifreUnButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
