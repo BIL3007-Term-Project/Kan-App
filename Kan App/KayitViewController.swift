455a390e335b6c6d9303842931b43f9cae89e4d6
HudahanAltn <dev.hudahanaltun@gmail.com>
2022-11-29 00:34:21 +0300

    Asset dosyaları eklendi.View Controller sayfaları ve sınıfları oluşturuldu

---
+++ Kan App/KayitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KayitViewController.swift
+//  Kan App
+//
+//  Created by Hüdahan Altun on 29.11.2022.
+//
+
+import UIKit
+
+class KayitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
